176c8579b824afc6402472e5f42a573718eede8e
prajwalbharadwaj <prajwalrb17@gmail.com>
2025-01-09 13:50:11 +0400

    minor

--- src/App.vue
+++ src/App.vue
@@ -23,8 +23,9 @@ watch(() => state.selectedCountry?.id, () => {
   <div class="container">
     <Dropdown v-model:selected="state.selectedCountry" :options="allCountries" placeholder="Select a country"
       class="w-full md:w-56" label="Select Country" />
-    <Dropdown v-if="state.selectedCountry?.id" :key="state.selectedCountry?.id" v-model:selected="state.selectedState"
-      :options="allStates" placeholder="Select a state" class="w-full md:w-56" label="Select State" />
+    <Dropdown v-if="state.selectedCountry?.id && allStates?.length" :key="state.selectedCountry?.id"
+      v-model:selected="state.selectedState" :options="allStates" placeholder="Select a state" class="w-full md:w-56"
+      label="Select State" />
     <Dropdown v-if="state.selectedState?.id && allCities?.length" v-model:selected="state.selectedCities"
       :options="allCities" placeholder="Select a City" class="w-full md:w-56" label="Select City" />
   </div>
